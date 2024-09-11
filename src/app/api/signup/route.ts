5ee2fa0f8e157c1c3fdffc94aa21275369241a6c
AreejAzeem <areejazeem67@gmail.com>
2024-09-11 09:07:07 +0500

    tweaks

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,24 +1,21 @@
 import bcrypt from 'bcryptjs';
 import { NextResponse } from 'next/server';
-import dbConnect from '@/lib/mongodb'; 
-import User from '@/app/models/User'; 
+import dbConnect from '@/lib/mongodb';
+import User from '@/app/models/User';
 
 export async function POST(request: Request) {
   await dbConnect();
 
   try {
     const { username, email, password } = await request.json();
-console.log('name:', username);
+    console.log('name:', username);
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return NextResponse.json({ message: 'User already exists' }, { status: 400 });
     }
-
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
-
     return NextResponse.json({ message: 'User created successfully' }, { status: 200 });
   } catch (error) {
     console.error('Error in POST /api/signup:', error);
